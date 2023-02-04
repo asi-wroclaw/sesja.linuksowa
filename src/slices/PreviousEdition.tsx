c755c3475d8e328238d8eff6a8f97a44ac36c305
Łukasz Burdyna <lukasz.burdyna@ocado.com>
2023-02-04 16:28:07 +0100

    Refactor - respond to the comments

--- src/slices/PreviousEdition.tsx
+++ src/slices/PreviousEdition.tsx
@@ -9,9 +9,9 @@ const PreviousEdition = () => {
         text: 'Zobacz poprzednie prelekcje',
         buttonText: 'PRZEJDŹ DO NAGRAŃ',
         youtubeUrl: 'https://www.youtube.com/embed/jMk-6SDVXC8',
+        youtubeIframeTitle: '16 Sesja Linuksowa',
         previousEditionUrl: 'https://16.sesja.linuksowa.pl/'
     }
-
 
     return <Box
         display="flex"
@@ -24,11 +24,15 @@ const PreviousEdition = () => {
 
         <Flex width="95%"
             marginY="auto"
-            direction={{ base: "column", lg: "row" }}
-            >
+            direction={{ base: "column", lg: "row" }}>
             <Center flex={1}>
                 <VStack spacing="3em">
-                    <Heading as="h2" fontSize={{ base: '4xl', md: '7xl' }} textAlign="center" fontWeight="semibold">{content.text}</Heading>
+                    <Heading as="h2"
+                        fontSize={{ base: '4xl', md: '7xl' }}
+                        textAlign="center"
+                        fontWeight="semibold">
+                        {content.text}
+                    </Heading>
                     <Link href={content.previousEditionUrl}>
                         <DefaultButton text={content.buttonText} />
                     </Link>
@@ -36,7 +40,13 @@ const PreviousEdition = () => {
             </Center>
             <Center flex={2} marginTop="3em">
                 <AspectRatio width="100%" ratio={16 / 9}>
-                    <iframe width="100%" height="100%" src={content.youtubeUrl} title="16 Sesja Linuksowa" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                    <iframe width="100%"
+                        height="100%"
+                        src={content.youtubeUrl}
+                        title={content.youtubeIframeTitle}
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowFullScreen>
+                    </iframe>
                 </AspectRatio>
             </Center>
         </Flex>
